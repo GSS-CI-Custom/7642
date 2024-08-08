f523d2c3371cc68d812ec08f63b9917b843a7b91
Angel Tudon <atudon@gssmail.com>
2024-08-07 19:01:12 -0500

    Commit changes

---
+++ Table Schema/GCG_7642_PL_OMIT.sql
@@ -0,0 +1,9 @@
+-- Database: GLOBALTST Metadata version: 2 Exported: Aug 6, 2024
+-- Begin Tables
+SET LEGACYTYPESALLOWED = ON;
+-- Begin Multi-Line Statement
+CREATE TABLE "GCG_7642_PL_OMIT" USING 'GCG_7642_PL_OMIT.mkd' PAGESIZE=4096 (
+ "PL" CHAR(2) );
+-- End Multi-Line Statement
+SET LEGACYTYPESALLOWED = OFF;
+-- End Tables
---
+++ Table Schema/GCG_7642_WC_BIN.sql
@@ -0,0 +1,10 @@
+-- Database: GLOBALTST Metadata version: 2 Exported: Aug 6, 2024
+-- Begin Tables
+SET LEGACYTYPESALLOWED = ON;
+-- Begin Multi-Line Statement
+CREATE TABLE "GCG_7642_WC_BIN" USING 'GCG_7642_WC_BIN.mkd' PAGESIZE=4096 (
+ "WORKCENTER" CHAR(4),
+ "BIN" CHAR(6) );
+-- End Multi-Line Statement
+SET LEGACYTYPESALLOWED = OFF;
+-- End Tables
